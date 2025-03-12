f471d88580648c1316d98367d882ba7e18e48312
kfnawaz <kfnawaz@gmail.com>
2025-03-12 08:07:50 +0000

    commit

--- client/src/components/admin/AdminNav.tsx
+++ client/src/components/admin/AdminNav.tsx
@@ -71,7 +71,7 @@ const AdminNav = ({ activePage }: AdminNavProps) => {
     },
     { 
       href: '/admin/subcontractors', 
-      label: 'Subcontractors & Vendors', 
+      label: 'Vendors', 
       icon: <Users className="w-5 h-5 mr-3" />,
       active: activePage === 'subcontractors'
     },
